17e2c839d211b49be004dc2c6e7c0d6d2345529f
sayandas24 <yosayan007@gmail.com>
2024-12-14 11:57:21 +0530

    add related videos

--- frontend/src/components/Video/RelatedVideos.jsx
+++ frontend/src/components/Video/RelatedVideos.jsx
@@ -1,130 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { NavLink } from "react-router";
 
 function RelatedVideos() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get("videos/")
+      .then((res) => {
+        setVideos(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(videos);
+
   return (
     <section className=" w-full rounded-2xl flex text-white flex-col gap-3">
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
+      {videos.map((video) => (
+        <div key={video._id} className="flex  gap-2 w-full">
+          {/* thumbnail */}
+          <NavLink to={`/p/${video._id}`}>
             <div className="w-[13rem] h-[8rem] border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUdNBSGl15RvUGCbz86mYbMe6T39f6F3HxOg&s" alt="" />
+              <img
+                className="h-full w-full object-cover"
+                src={video.thumbnail}
+                alt=""
+              />
             </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
+          </NavLink>
+
+          {/* thumbnail details */}
+          <div className="flex flex-col py-2 gap-1 w-[25rem]">
+            <h1 className="text-[1.2rem] font-semibold text-wrap">
+              {video.title}
+            </h1>
+
+            <div className="flex flex-col text-zinc-400 text-[1rem]">
+              <h2 className="text-zinc-300">{video.owner.fullName}</h2>
+              <div className="flex gap-2 items-center">
+                <span>45M views</span>
+                <span>2 years ago</span>
+              </div>
             </div>
           </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPzcytwkvX_o4_F8T_3eL2enUfPZH29ga2ML3vPBJpvKt74NHww3IyEm-DKkNw8DTZ_xg&usqp=CAU" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-          <div className="flex   gap-2 w-full">
-            {/* thumbnail */}
-            <div className="w-[13rem] h-[8rem]  border-zinc-600 rounded-2xl overflow-hidden">
-              <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/52/6a/b2/526ab2346bc63129193fb9b86b60d24b.jpg" alt="" />
-            </div>
-            {/* thumbnail details */}
-            <div className="flex flex-col py-2">
-              <h1 className="text-[1rem] font-semibold">Title of the video</h1>
-              <h2 className="flex flex-col text-zinc-400 text-[.8rem]">
-                <span>Channel name</span>
-                <span>69K views</span>
-                <span>1 day ago</span>
-              </h2>
-            </div>
-          </div>
-        </section>
-  )
+        </div>
+      ))}
+    </section>
+  );
 }
 
-export default RelatedVideos
+export default RelatedVideos;
