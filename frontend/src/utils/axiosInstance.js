8771ff27af80ce2bfdf906e3da592fdd2b45eeb8
sayandas24 <yosayan007@gmail.com>
2024-12-31 11:25:19 +0530

    feat: update axios instance base URL to production server

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // Replace with your backend's base URL
+  baseURL: "https://metube-n3vg.onrender.com/api/v1"
+  // "http://localhost:8000/api/v1",  
   // "https://metube-n3vg.onrender.com/api/v1",
 });
 
